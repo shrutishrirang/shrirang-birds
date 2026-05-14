1e659b3b10f8982fea035e0b9267d6c5e8d9b88e
Shruti Kulkarni <shrutishrirang@outlook.com>
2026-05-14 11:08:39 +0530

    fix studio build error
    :wq:wq#

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,7 +1,12 @@
-import { NextStudio } from 'next-sanity/studio'
+'use client'
+
+import dynamic from 'next/dynamic'
 import config from '@/sanity.config'
 
-export { metadata, viewport } from 'next-sanity/studio'
+const NextStudio = dynamic(
+  () => import('next-sanity/studio').then(mod => ({ default: mod.NextStudio })),
+  { ssr: false }
+)
 
 export default function StudioPage() {
   return <NextStudio config={config} />
